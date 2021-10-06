c572f9fd3120f98043ab8bd2940948143f9332f5
rafaelalbia <rafaelalbia@outlook.com>
2021-10-06 07:42:10 -0300

    Import of spring5webapp classes

---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+}
---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring5webapp.domain;
+
+public class Book {
+    private String title;
+    private String isbn;
+}
