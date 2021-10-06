d2c6e4ff3c55f8b5c4eb2a38a3863e425d5e2cce
rafaelalbia <rafaelalbia@outlook.com>
2021-10-06 14:42:43 -0300

    Implementation of Interfaces

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,7 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
 import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
