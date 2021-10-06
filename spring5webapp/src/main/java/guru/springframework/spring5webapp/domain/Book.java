ba738ee55abc611134e47d9b883b7dee6b2bba3f
rafaelalbia <rafaelalbia@outlook.com>
2021-10-06 15:35:13 -0300

    Initializing Data

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,10 +1,13 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 
@@ -18,16 +21,17 @@ public class Book {
     private String title;
     private String isbn;
     
-    @ManyToMany(mappedBy = "authors")
-    private Set<Author> authors;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
     
 	public Book() {
 	}
 
-	public Book(String title, String isbn, Set<Author> authors) {
+	public Book(String title, String isbn) {
 		this.title = title;
 		this.isbn = isbn;
-		this.authors = authors;
 	}
 
 	public Long getId() {
