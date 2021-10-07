f5d4da916938ae6444d0dfa1835b16ac62714e1c
rafaelalbia <rafaelalbia@outlook.com>
2021-10-07 07:37:28 -0300

    Creation of Publisher class | ManyToOne and OneToMany relationship | Deletion of .gitignore file

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,18 +14,22 @@ public class BootStrapData implements CommandLineRunner {
 	
 	private final AuthorRepository authorRepository;
 	private final BookRepository bookRepository;
+	private final PublisherRepository publisherRepository;
 
-	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		Publisher nature = new Publisher("Nature Publishing Group", "One New York Plaza, Suite 4500", "New York City", "New York", "100041562");
 		Author eric = new Author("Eric", "Evans");
 		Book ddd = new Book("Domain-Driven Design", "9780321125217");
 		eric.getBooks().add(ddd);
 		ddd.getAuthors().add(eric);
+		nature.getBooks().add(ddd);
 		
 		authorRepository.save(eric);
 		bookRepository.save(ddd);
@@ -32,11 +38,16 @@ public class BootStrapData implements CommandLineRunner {
 		Book noEJB = new Book("J2EE Development without EJB", "9780470332962");
 		rod.getBooks().add(noEJB);
 		noEJB.getAuthors().add(rod);
+		nature.getBooks().add(noEJB);
 		
 		authorRepository.save(rod);
 		bookRepository.save(noEJB);
+		publisherRepository.save(nature);
 		
 		System.out.println("Started in Bootstrap");
+		System.out.println("Number of Publishers: " + publisherRepository.count());
 		System.out.println("Number of Books: " + bookRepository.count());
+		System.out.println("Number of books published by Nature Publishing Group: " + nature.getBooks().size());
+		
 	}
 }
