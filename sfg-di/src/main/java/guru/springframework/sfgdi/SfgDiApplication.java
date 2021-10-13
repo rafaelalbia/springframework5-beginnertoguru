b272a3fab813144f2507ae2b7ca58aa3a387fb9d
rafaelalbia <rafaelalbia@outlook.com>
2021-10-13 07:23:08 -0300

    Creation of PetController controller | Creation of pets services

--- sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgdi;
 import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.I18nController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PetController;
 import guru.springframework.sfgdi.controllers.PropertyInjectedController;
 import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +15,10 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("----- The Best Pet is");
+		System.out.println(petController.whichPetIsTheBest());
 		
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
 		System.out.println(i18nController.sayHello());
---
+++ sfg-di/src/main/java/guru/springframework/sfgdi/services/PetService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgdi.services;
+
+public interface PetService {
+
+	String getPetType();
+	
+}
