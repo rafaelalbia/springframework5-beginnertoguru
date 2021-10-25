b68b065ca90384374b51946e8de4ec1acea680f4
rafaelalbia <rafaelalbia@outlook.com>
2021-10-25 14:48:27 -0300

    Adding BaseEntity | Closes #9

--- sfgpetclinic/petclinicdata/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ sfgpetclinic/petclinicdata/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 	
 	private String firstName;
     private String lastName;
@@ -8,12 +8,15 @@ public class Person {
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
